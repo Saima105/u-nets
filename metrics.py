ca392e5f871191f543e1ba7273d6bf775d22ba27
Saima105 <69521802+Saima105@users.noreply.github.com>
2021-08-30 20:01:49 +0530

    Add files via upload

---
+++ metrics.py
@@ -0,0 +1,67 @@
+import numpy as np
+from scipy import spatial
+
+SMOOTH = 1e-6
+def dice_coeff(im1, im2, empty_score=1.0):
+    """Calculates the dice coefficient for the images"""
+
+    im1 = np.asarray(im1).astype(np.bool)
+    im2 = np.asarray(im2).astype(np.bool)
+
+    if im1.shape != im2.shape:
+        raise ValueError("Shape mismatch: im1 and im2 must have the same shape.")
+
+    im1 = im1 > 0.5
+    im2 = im2 > 0.5
+
+    im_sum = im1.sum() + im2.sum()
+    if im_sum == 0:
+        return empty_score
+
+    # Compute Dice coefficient
+    intersection = np.logical_and(im1, im2)
+    #print(im_sum)
+
+    return 2. * intersection.sum() / im_sum
+
+
+def numeric_score(prediction, groundtruth):
+    """Computes scores:
+    FP = False Positives
+    FN = False Negatives
+    TP = True Positives
+    TN = True Negatives
+    return: FP, FN, TP, TN"""
+    prediction = prediction > 0.6
+    prediction = prediction.astype(int)
+    FP = np.float(np.sum((prediction == 1) & (groundtruth == 0)))
+    FN = np.float(np.sum((prediction == 0) & (groundtruth == 1)))
+    TP = np.float(np.sum((prediction == 1) & (groundtruth == 1)))
+    TN = np.float(np.sum((prediction == 0) & (groundtruth == 0)))
+
+    return FP, FN, TP, TN
+
+
+def accuracy_score(prediction, groundtruth):
+    """Getting the accuracy of the model"""
+
+    FP, FN, TP, TN = numeric_score(prediction, groundtruth)
+    N = FP + FN + TP + TN
+    accuracy = np.divide(TP + TN, N)
+    Precision = np.divide(TP,TP+FP)
+    Recall = np.divide(TP,TP+FN)
+    f1_scroe = np.divide(2*np.multiply(Precision,Recall),Precision+Recall)
+    return accuracy, Precision, Recall,f1_scroe
+
+def iou_numpy(outputs: np.array, labels: np.array):
+    outputs = outputs > 0.6
+    outputs = outputs.squeeze(1)
+    
+    intersection = (outputs & labels).sum((1, 2))
+    union = (outputs | labels).sum((1, 2))
+    
+    iou = (intersection + SMOOTH) / (union + SMOOTH)
+    
+    thresholded = np.ceil(np.clip(20 * (iou - 0.5), 0, 10)) / 10
+    
+    return thresholded.mean()  # Or thresholded.mean()
